69d09e897429bc454e153411299e0b020a0f0c1e
Steve Jackson <steve@testdouble.com>
2019-12-05 16:12:07 -0600

    Day 2 - handle opcodes

--- 2019/a2/lib/a2.ex
+++ 2019/a2/lib/a2.ex
@@ -3,20 +3,22 @@ defmodule A2 do
   Documentation for A2.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> A2.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def next_instruction(opcodes) do
     opcodes |> Enum.slice(0, 4)
+  end
+
+  def handle_instruction(opcode, input) do
+    case opcode do
+      [1, in1, in2, out] ->
+        sum = Enum.at(input, in1) + Enum.at(input, in2)
+        output = List.replace_at(input, out, sum)
+        {:ok, output}
+      [2, in1, in2, out] ->
+        product = Enum.at(input, in1) * Enum.at(input, in2)
+        output = List.replace_at(input, out, product)
+        {:ok, output}
+      [99|_] ->
+        {:done, input}
+    end
   end
 end
--- 2019/a2/test/a2_test.exs
+++ 2019/a2/test/a2_test.exs
@@ -1,11 +1,6 @@
 defmodule A2Test do
   use ExUnit.Case
   doctest A2
-
-  test "greets the world" do
-    assert A2.hello() == :world
-  end
-
   test "next_instruction simple" do
     assert A2.next_instruction([1, 9, 10, 3]) == [1, 9, 10, 3]
   end
@@ -19,4 +14,25 @@ defmodule A2Test do
     # I'm ok with this for now - let the instruction processor figure it out
     assert A2.next_instruction([99, 1, 9, 10, 3, 44]) == [99, 1, 9, 10]
   end
+
+  test "opcode 1" do
+    program = [1, 1, 1, 1]
+    assert A2.handle_instruction([1, 1, 1, 1], program) == {:ok, [1, 2, 1, 1]}
+  end
+
+  test "write out of bounds on opcode 1 does nothing?" do
+    program = [1, 1, 1, 42]
+    assert A2.handle_instruction([1, 1, 1, 42], program) == {:ok, [1, 1, 1, 42]}
+  end
+
+  test "opcode2" do
+    program = [2, 1, 1, 1]
+    assert A2.handle_instruction([2, 1, 1, 1], program) == {:ok, [2, 1, 1, 1]}
+  end
+
+  test "opcode 99" do
+    program = [2, 1, 1, 1]
+    assert A2.handle_instruction([99], program) == {:done, program}
+  end
+
 end
