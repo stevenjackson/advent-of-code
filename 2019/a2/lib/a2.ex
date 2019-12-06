37a7645911ff84f036494752eabe7e738c64aafc
Steve Jackson <steve@testdouble.com>
2019-12-05 18:16:43 -0600

    Day 2 - Finish part 1

--- 2019/a2/lib/a2.ex
+++ 2019/a2/lib/a2.ex
@@ -3,8 +3,8 @@ defmodule A2 do
   Documentation for A2.
   """
 
-  def next_instruction(opcodes) do
-    opcodes |> Enum.slice(0, 4)
+  def next_instruction(index, opcodes) do
+    opcodes |> Enum.slice(index, 4)
   end
 
   def handle_instruction(opcode, input) do
@@ -19,6 +19,44 @@ defmodule A2 do
         {:ok, output}
       [99|_] ->
         {:done, input}
+      _ ->
+        {:error, input}
     end
+  end
+
+  def run_program(program) do
+    run_program(0, program)
+  end
+
+  def run_program(index, program) do
+    instruction = next_instruction(index, program)
+    result = handle_instruction(instruction, program)
+    case result do
+      {:ok, program} ->
+        index = index + length(instruction)
+        run_program(index, program)
+       _ ->
+         result
+    end
+  end
+
+  def read_program(filename) do
+    {:ok, contents} = File.read(filename)
+   contents
+    |> String.trim
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def solve_part_1(filename) do
+    read_program(filename)
+    |> modify_program
+    |> run_program
+  end
+
+  def modify_program(program) do
+    program
+    |> List.replace_at(1, 12)
+    |> List.replace_at(2, 2)
   end
 end
--- 2019/a2/test/a2_test.exs
+++ 2019/a2/test/a2_test.exs
@@ -2,17 +2,17 @@ defmodule A2Test do
   use ExUnit.Case
   doctest A2
   test "next_instruction simple" do
-    assert A2.next_instruction([1, 9, 10, 3]) == [1, 9, 10, 3]
+    assert A2.next_instruction(0, [1, 9, 10, 3]) == [1, 9, 10, 3]
   end
 
   test "next_instruction simplish" do
-    assert A2.next_instruction([1, 9, 10, 3, 44]) == [1, 9, 10, 3]
+    assert A2.next_instruction(0, [1, 9, 10, 3, 44]) == [1, 9, 10, 3]
   end
 
   test "next_instruction 99" do
-    assert A2.next_instruction([99]) == [99]
+    assert A2.next_instruction(0, [99]) == [99]
     # I'm ok with this for now - let the instruction processor figure it out
-    assert A2.next_instruction([99, 1, 9, 10, 3, 44]) == [99, 1, 9, 10]
+    assert A2.next_instruction(0, [99, 1, 9, 10, 3, 44]) == [99, 1, 9, 10]
   end
 
   test "opcode 1" do
@@ -35,4 +35,29 @@ defmodule A2Test do
     assert A2.handle_instruction([99], program) == {:done, program}
   end
 
+  test "bad opcode" do
+    program = [2, 1, 1, 1]
+    assert A2.handle_instruction([4, 1, 1, 1], program) == {:error, program}
+  end
+
+  test "one instruction" do
+    program = [2, 1, 1, 1, 99]
+    assert A2.run_program(program) == {:done, [2, 1, 1, 1, 99]}
+  end
+
+  test "two instruction" do
+    program = [1, 1, 1, 1, 2, 1, 1, 1, 99]
+    assert A2.run_program(program) == {:done, [1, 4, 1, 1, 2, 1, 1, 1, 99]}
+  end
+
+  test "bad program" do
+    program = [1, 1, 1, 1, 2, 1, 1, 1, 3, 1, 1, 1, 99]
+    assert A2.run_program(program) == {:error, [1, 4, 1, 1, 2, 1, 1, 1, 3, 1, 1, 1, 99]}
+  end
+
+  test "solve_part_1" do
+    result =
+    {:done, [head | tail]} = A2.solve_part_1('input.txt')
+    assert head == 3716250
+  end
 end
