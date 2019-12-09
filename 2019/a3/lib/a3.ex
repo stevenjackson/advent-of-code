90f93b55037aedcfc8306e6a93993d737c1e794d
Steve Jackson <steve@testdouble.com>
2019-12-08 18:12:47 -0600

    Day 3 - Part 1

---
+++ 2019/a3/lib/a3.ex
@@ -0,0 +1,52 @@
+defmodule A3 do
+  @moduledoc """
+  Documentation for A3.
+  """
+
+  def points(schematic) do
+    String.split(schematic, ",", trim: true)
+    |> Enum.reduce([{0, 0}], fn direction, points ->
+      starting = List.last(points)
+      points ++ move(starting, direction)
+    end
+    )
+  end
+
+  def move({x, y}, direction) do
+    <<dir::utf8, distance::bytes>> = direction
+    distance = String.to_integer(distance)
+    case dir do
+      ?U -> for up <- 1..distance, do: {x, y + up}
+      ?D -> for down <- 1..distance, do: {x, y - down}
+      ?R -> for right <- 1..distance, do: {x + right, y}
+      ?L -> for left <- 1..distance, do: {x - left, y}
+    end
+  end
+
+  def intersections(schematic1, schematic2) do
+    points1 = points(schematic1)
+    points2 = points(schematic2)
+    points1 -- (points1 -- points2)
+  end
+
+  def find_closest(points, point) do
+    points -- [point]
+    |> Enum.min_by(fn p -> distance(point, p) end)
+  end
+
+  def distance({x1, y1}, {x2, y2}) do
+    abs(x1 - x2) + abs(y1 - y2)
+  end
+
+  def find_distance_to_closest_intersection(schematic1, schematic2) do
+   intersections(schematic1, schematic2)
+   |> find_closest({0, 0})
+   |> distance({0, 0})
+  end
+
+
+  def read_schematics(filename) do
+    {:ok, contents} = File.read(filename)
+    contents |> String.split("\n", trim: true)
+  end
+end
---
+++ 2019/a3/test/a3_test.exs
@@ -0,0 +1,52 @@
+defmodule A3Test do
+  use ExUnit.Case
+  doctest A3
+
+  test "move" do
+    assert A3.move({0, 0}, "U1") == [{0, 1}]
+  end
+
+  test "move more" do
+    assert A3.move({0, 0}, "L3") == [{-1, 0}, {-2, 0}, {-3, 0}]
+  end
+
+  test "find_points" do
+    wire = "R1,D1"
+    points = [{0, 0}, {1, 0}, {1, -1}]
+    assert A3.points(wire) == points
+  end
+
+  test "intersections" do
+    wire1 = "R1,D1"
+    wire2 = "D1,R1"
+    assert A3.intersections(wire1, wire2) == [{0, 0}, {1, -1}]
+  end
+
+  test "find_closest" do
+    assert A3.find_closest([{-4, 3}, {5, 4}], {0, 0}) == {-4, 3}
+  end
+
+  test "find_closest filters out exact match" do
+    assert A3.find_closest([{0, 0}, {-4, 3}, {5, 4}], {0, 0}) == {-4, 3}
+  end
+
+  test "distance check" do
+    wire1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+    wire2 = "U62,R66,U55,R34,D71,R55,D58,R83"
+
+    assert A3.find_distance_to_closest_intersection(wire1, wire2) == 159
+  end
+
+  test "distance check2" do
+    wire1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+    wire2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+
+    assert A3.find_distance_to_closest_intersection(wire1, wire2) == 135
+  end
+
+  test "solve1" do
+    [wire1, wire2] = A3.read_schematics('input.txt')
+    distance = A3.find_distance_to_closest_intersection(wire1, wire2)
+    IO.puts "problem 1: #{distance}"
+  end
+end
