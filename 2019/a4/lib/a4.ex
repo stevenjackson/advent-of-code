db5c4b29319f9aeae4036f360b867e0a8c9fd1cb
Steve Jackson <steve@testdouble.com>
2020-01-12 16:58:22 -0500

    Day 4 - Part 2

--- 2019/a4/lib/a4.ex
+++ 2019/a4/lib/a4.ex
@@ -1,19 +1,9 @@
 defmodule A4 do
-
-  def find_matches(first, second) do
-    (first..second)
-    |> Enum.filter(&digits_increasing?/1)
-    |> Enum.filter(&double_digits?/1)
-  end
-
   def double_digits?(value) do
     double_digits = Integer.digits(value)
      |> Enum.reduce_while(-1, fn digit, prev_digit ->
        if digit == prev_digit, do: {:halt, true}, else: {:cont, digit}
      end)
-
-    double_digits == true
-  end
 
     double_digits == true
   end
@@ -25,5 +15,15 @@ defmodule A4 do
      end)
 
     decreasing != false
+  end
+
+  def has_double?(value) do
+    find_matches(value)
+    |> Enum.any?(fn x -> length(x) == 2 end)
+  end
+
+  def find_matches(value) do
+    Integer.digits(value)
+    |> Enum.chunk_by(fn(x) -> x end)
   end
 end
--- 2019/a4/test/a4_test.exs
+++ 2019/a4/test/a4_test.exs
@@ -14,13 +14,33 @@ defmodule A4Test do
     assert A4.digits_increasing?(379)
   end
 
-  test "find_matches" do
-    assert A4.find_matches(121, 123) == [122]
+  test "finding matches" do
+    matches = (121..123)
+      |> Enum.filter(&A4.digits_increasing?/1)
+      |> Enum.filter(&A4.double_digits?/1)
+    assert matches == [122]
   end
 
   test "Solution 1" do
-    matches = A4.find_matches(138241, 674034)
+    matches = (138241..674034)
+      |> Enum.filter(&A4.digits_increasing?/1)
+      |> Enum.filter(&A4.double_digits?/1)
     IO.inspect  matches
-    IO.inspect length(matches)
+    IO.puts "problem 1: #{length(matches)}"
+  end
+
+  test "has_double" do
+    assert A4.has_double?(11)
+    refute A4.has_double?(111)
+    assert A4.has_double?(11122)
+  end
+
+  test "Solution 2" do
+    matches = (138241..674034)
+      |> Enum.filter(&A4.digits_increasing?/1)
+      |> Enum.filter(&A4.double_digits?/1)
+      |> Enum.filter(&A4.has_double?/1)
+    IO.inspect  matches
+    IO.puts "problem 2: #{length(matches)}"
   end
 end
